3269d91031277aa42f4712e489f219f808d2504c
catalinbalanescu <62386248+catalinbalanescu@users.noreply.github.com>
2022-05-16 19:21:42 +0300

    Add files via upload
    Login1

---
+++ Login.java
@@ -0,0 +1,36 @@
+package org.loose.fis.registration.example.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.loose.fis.registration.example.exceptions.UsernameAlreadyExistsException;
+import org.loose.fis.registration.example.services.UserService;
+
+public class Login {
+
+    @FXML
+    private Text LoginMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private ChoiceBox role;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Teacher", "Student");
+    }
+
+    @FXML
+    public void handleRegisterAction() {
+        try {
+            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+            LoginMessage.setText("Login successfully!");
+        } catch (UsernameAlreadyExistsException e) {
+            LoginMessage.setText(e.getMessage());
+        }
+    }
+}
